dd2f63c8a67c53d84e5e56b9b8e8f2c9c5ac00c0
DeepiArun <deepikaunkumarr@gmail.com>
2023-09-17 01:39:03 +0300

    modified app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-import { serve } from "./deps.js";
-import { configure, renderFile } from "./deps.js";
+import { serve } from "https://deno.land/std/http/server.ts";
+import { configure, renderFile } from "https://deno.land/x/dejs/mod.ts";
 
 configure({
   views: `${Deno.cwd()}/views/`,
 });
 
-const responseDetails = {
-  headers: { "Content-Type": "text/html;charset=UTF-8" },
-};
+const server = serve({ port: 7777 });
 
+for await (const request of server) {
+  const url = new URL(request.url);
+
+  if (url.pathname === "/count") {
 const data = {
   count: 0,
 };
@@ -17,16 +19,32 @@ const handleRequest = async (request) => {
   const url = new URL(request.url);
   if (url.pathname === "/count") {
     data.count++;
-    return new Response(await renderFile("count.eta", data), responseDetails);
+    const body = await renderFile("count.eta", data);
+    const responseHeaders = new Headers({
+      "Content-Type": "text/html; charset=UTF-8",
+    });
+    request.respond({ body, headers: responseHeaders });
+  } else if (url.pathname === "/meaning") {
+    const body = "Seeking truths beyond the meaning of life, you will find 43.";
+    const responseHeaders = new Headers({
+      "Content-Type": "text/html; charset=UTF-8",
+    });
+    request.respond({ body, headers: responseHeaders });
+  } else {
+    const body = "Nothing here yet.";
+    const responseHeaders = new Headers({
+      "Content-Type": "text/html; charset=UTF-8",
+    });
+    request.respond({ body, headers: responseHeaders });
   }
-  else if (url.pathname === "/meaning")
-  {
-    request.respond({ body: "Seeking truths beyond the meaning of life, you will find 43.", headers: responseHeaders });
+  } 
   } 
   else 
   {
     request.respond({ body: "Nothing here yet.", headers: responseHeaders });
   }
+  
+    request.respond({ body: "Nothing here yet.", headers: responseHeaders });
 };
 
 serve(handleRequest, { port: 7777 });

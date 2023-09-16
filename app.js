63ad1fd436b8a1189e1d3f39cfc2ef19be4fcbe4
DeepiArun <deepikaunkumarr@gmail.com>
2023-09-17 02:19:08 +0300

    changed to old code

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ configure({
   views: `${Deno.cwd()}/views/`,
 });
 
-const server = serve({ port: 7777 });
+const responseDetails = {
+  headers: { "Content-Type": "text/html;charset=UTF-8" },
+};
 
-for await (const request of server) {
-  const url = new URL(request.url);
-
-  if (url.pathname === "/count") {
 const data = {
   count: 0,
 };
@@ -19,31 +17,10 @@ const handleRequest = async (request) => {
   const url = new URL(request.url);
   if (url.pathname === "/count") {
     data.count++;
-    const body = await renderFile("count.eta", data);
-    const responseHeaders = new Headers({
-      "Content-Type": "text/html; charset=UTF-8",
-    });
-    request.respond({ body, headers: responseHeaders });
-  } else if (url.pathname === "/meaning") {
-    const body = "Seeking truths beyond the meaning of life, you will find 43.";
-    const responseHeaders = new Headers({
-      "Content-Type": "text/html; charset=UTF-8",
-    });
-    request.respond({ body, headers: responseHeaders });
-  } else {
-    const body = "Nothing here yet.";
-    const responseHeaders = new Headers({
-      "Content-Type": "text/html; charset=UTF-8",
-    });
-    request.respond({ body, headers: responseHeaders });
+    return new Response(await renderFile("count.eta", data), responseDetails);
   }
-  } 
-  } 
-  else 
-  {
-    request.respond({ body: "Nothing here yet.", headers: responseHeaders });
-  }
-  
+
+  return new Response("Hello you!");
 };
 
 serve(handleRequest, { port: 7777 });

896dc5d4ce50b16ed080cac708539ac48a7b3466
DeepiArun <deepikaunkumarr@gmail.com>
2023-09-19 16:53:11 +0300

    added life.eta file

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
 import { serve } from "./deps.js";
 import { configure, renderFile } from "./deps.js";
 
+ 
+
 configure({
-  views: `${Deno.cwd()}/views/`, 
+  views: `${Deno.cwd()}/views/`,
 });
+
+ 
 
 const responseDetails = {
   headers: { "Content-Type": "text/html;charset=UTF-8" },
 };
 
-const data = 
-{
+ 
+
+const data = {
   count: 0,
-}
-const handleRequest = async (request) => {
-  const url = new URL(request.url);
-  if (url.pathname === "/visits") 
-  {
-    data.count++;
-    
-    return new Response(await renderFile("visits.eta", data), responseDetails);
-
-  } 
-  else if (url.pathname === "/meaning")
-   {
-    data.count++;
-    return new Response(await renderFile("life.eta"),responseDetails);
-    } 
-
-    return new Response("Nothing here yet.");
-  
 };
 
-serve(handleRequest, { port: 7777 });
+const handleRequest = async (request) => {
+  const url = new URL(request.url);
+  if (url.pathname === "/visits") {
+    data.count++;
+    return new Response(await renderFile("visits.eta", data), responseDetails);
+  } else if (url.pathname === "/meaning") {
+    data.count++;
+    return new Response(await renderFile("life.eta", data), responseDetails);
+  }
+  return new Response("Nothing here yet.");
+};
+
+serve(handleRequest, { port: 7777 }); 
